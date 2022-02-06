d3420f78c3057ed7bb76e93935ba1393f2a2b146
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-02-06 18:18:25 +0300

    feature:input function into js file

--- js/jscript.js
+++ js/jscript.js
@@ -1,8 +1,24 @@
 function myfunction(){
-    score = 0;
-    document.getElementById("correct1").innerHTML = "Correct"
-
-
-
-
+    var score=0;
+    if (document.getElementById("correct1").checked)
+    {
+        score++;
+    }
+    if (document.getElementById("correct2").checked)
+    {
+        score++;
+    }
+    if (document.getElementById("correct3").checked)
+    {
+        score++;
+    }
+    if (document.getElementById("correct4").checked)
+    {
+        score++;
+    }
+    if (document.getElementById("correct5").checked)
+    {
+        score++;
+    }
+    alert("Your score is:"+score)
 }
