ac7db427c8413162c0c0b4deae784015afab1130
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-02-08 03:52:45 +0300

    add p tag with #scorep

--- js/jscript.js
+++ js/jscript.js
@@ -20,15 +20,19 @@ function myfunction(){
     {
         score++;
     }
-alert("Your score is:"+score)}
 
-var customerName = prompt("Please enter your name", "<name goes here>");
-
-if (customerName!= null) {
-
-    document.getElementById("welcome").innerHTML =
-
-    "Hello " + customerName + "! How are you today?";
-
+    document.getElementById("scorep").innerHTML = score
 }
+
+
+
+// var customerName = prompt("Please enter your name", "<name goes here>");
+
+// if (customerName!= null) {
+
+//     document.getElementById("welcome").innerHTML =
+
+//     "Hello " + customerName + "! How are you today?";
+
+// }
 
